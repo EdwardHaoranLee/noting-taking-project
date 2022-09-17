4fbf4c9c04ec85414f922b2cfde795479ad0c350
Haoran Li <36394576+EdwardHaoranLee@users.noreply.github.com>
2022-09-17 09:41:57 -0400

    Add handlers file

---
+++ handler/note_to_quiz_converter.py
@@ -0,0 +1,10 @@
+from entity.note.note import Note
+from entity.question.question import Question
+
+
+class NoteToQuizConverter:
+    def __init__(self):
+        pass
+
+    def convert(self, note: Note) -> Question:
+        pass

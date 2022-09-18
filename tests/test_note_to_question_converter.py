02b79a138a3099d3a64e9734f8e011b56a164d1a
Haoran Li <36394576+EdwardHaoranLee@users.noreply.github.com>
2022-09-17 23:08:59 -0400

    lint

--- tests/test_note_to_question_converter.py
+++ tests/test_note_to_question_converter.py
@@ -14,8 +14,7 @@ class TestNoteToQuestionConverter(unittest.TestCase):
                                         "This causes hyperpolarization in the cell body",
                                         "Reduction of neurotransmitter glutamate at the synaptic level"])
         question, answer = self.converter.convert(note_example, "multiple_choice")
-        print("Question:\n" + str(question))
-        print("Answer: \n" + str(answer))
+        print()
 
     def test_bullet_point_note_with_short_answer_question(self):
         note_example = BulletPointNote("Biochemical cascade of events",
@@ -23,12 +22,10 @@ class TestNoteToQuestionConverter(unittest.TestCase):
                                         "This causes hyperpolarization in the cell body",
                                         "Reduction of neurotransmitter glutamate at the synaptic level"])
         question, answer = self.converter.convert(note_example, "short_answer")
-        print("Question:\n" + str(question))
-        print("Answer: \n" + str(answer))
+        print()
 
     def test_definition_note_with_short_answer_question(self):
         note_example = DefinitionNote("Pneumoconiosis",
                                       "A group of lung diseases caused by the lung's reaction inhaling certain dusts.")
         question, answer = self.converter.convert(note_example, "short_answer")
-        print("Question:\n" + str(question))
-        print("Answer: \n" + str(answer))
+        print()

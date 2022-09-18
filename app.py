e7738c232ea14c9e8c0ce196716e7d851e1178c3
Haoran Li <36394576+EdwardHaoranLee@users.noreply.github.com>
2022-09-17 22:48:33 -0400

    Implement createQuestion

--- app.py
+++ app.py
@@ -1,20 +1,45 @@
-from flask import Flask
+import json
+import random
+import atexit
+
+from flask import Flask, request
+
 from config import Config
+from db.db import DB
+from entity.note.bullet_point_note import BulletPointNote
+from entity.note.definition_note import DefinitionNote
+from entity.note.note import Note
+from handler.note_to_question_converter import NoteToQuestionConverter
 
 app = Flask(__name__)
+converter = NoteToQuestionConverter()
+db = DB()
+atexit.register(db.close)
 
 
 @app.route('/createQuiz')
 def createQuiz():
-    # Process the request data and convert to a note object
-    # Create quiz question from note object
-    # Return the quiz question
-    pass
+    note = parseNote(str(request.data))
+    if type(note.body) == str or random.randint(0, 1) == 0:
+        question, answer = converter.convert(note, "short_answer")
+    else:
+        question, answer = converter.convert(note, "multiple_choice")
+
+    db.connect()
+    uuid = db.save_question(question, answer)
+
+    obj = {
+        "uuid": uuid,
+        "question": question
+    }
+
+    return json.dumps(obj.__dict__)
 
 
 @app.route('/checkAnswer')
 def checkAnswer():
     pass
+
 
 @app.route('/')
 def checkConfig():
@@ -22,6 +47,20 @@ def checkConfig():
     print('COHERE_API_KEY:  {}'.format(Config.COHERE_API_KEY))
     print('HOSTNAME: {}'.format(Config.HOSTNAME))
     print('PORT:     {}'.format(Config.PORT))
+
+
+def parseNote(text: str) -> Note:
+    lines = list(map(lambda x: str(x), text.splitlines()))
+    if len(lines) == 1 and lines[0].find(":") != -1:
+        parts = lines[0].split(":")
+        assert len(parts) == 2
+        note = DefinitionNote(parts[0], parts[1])
+    elif len(lines) >= 2:
+        note = BulletPointNote(lines[0].strip(),
+                               list(map(lambda x: x.strip(), lines[1:])))
+    else:
+        raise AttributeError("Cannot parse request data.")
+    return note
 
 
 if __name__ == '__main__':
